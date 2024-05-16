84012e2053d4f17d05f13f5de0e9cb114032dfe1
Wojciech Przybyła <contact@wpww.pl>
2024-05-16 23:24:17 +0200

    🎨 set up vue scaffolding

---
+++ resources/js/Layout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header>
+        <h1>Magazyn</h1>
+    </header>
+
+    <slot />
+
+    <footer>
+        <span>For Promovera by Wojciech Przybyła</span>
+    </footer>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Link href="/stock">Stany magazynowe</Link>
+</template>
