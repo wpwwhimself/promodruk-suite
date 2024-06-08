2596ff4694639dd365db5a4c5575c2c581545f33
Wojciech Przybyła <contact@wpww.pl>
2024-06-08 14:56:42 +0200

    💄 adjusted table layout and spacing

--- resources/views/stock.blade.php
+++ resources/views/stock.blade.php
@@ -11,43 +11,38 @@
 </div>
 
 <style>
-.row {
-    grid-template-columns: 1fr 4fr 2fr 2fr 3fr;
+.table {
+    --col-count: 5;
+    grid-template-columns: repeat(var(--col-count), auto);
 }
 </style>
 <div class="table">
-    <div class="row head">
-        <span>Kod</span>
-        <span>Nazwa</span>
-        <span>Kolor</span>
-        <span>Akt. stan mag.</span>
-        <span>Planowana dostawa</span>
-    </div>
+    <span class="head">Kod</span>
+    <span class="head">Nazwa</span>
+    <span class="head">Kolor</span>
+    <span class="head">Akt. stan mag.</span>
+    <span class="head">Planowana dostawa</span>
     <hr>
     @forelse ($data as $row)
-    <div class="row">
-        <span>{{ $row["index"] }}</span>
-        <span>
-            <img
-                src="{{ collect($row["image"])
-                    ->sortBy("url")
-                    ->first()["url"]
-                }}"
-                alt="{{ collect($row["names"])->first(fn ($el) => $el["language"] == "pl")["title"] }}"
-                class="inline"
-            >
-            {{ collect($row["names"])->first(fn ($el) => $el["language"] == "pl")["title"] }}
-        </span>
-        <span>{{ collect($row["additional"])->first(fn ($el) => $el["item"] == "color_product")["value"] }}</span>
-        <b>{{ $row["quantity"] }} szt.</b>
-        <span>{{ processFutureDelivery($row["future_delivery"]) }}</span>
-    </div>
+    <span>{{ $row["index"] }}</span>
+    <span>
+        <img
+            src="{{ collect($row["image"])
+                ->sortBy("url")
+                ->first()["url"]
+            }}"
+            alt="{{ collect($row["names"])->first(fn ($el) => $el["language"] == "pl")["title"] }}"
+            class="inline"
+        >
+        {{ collect($row["names"])->first(fn ($el) => $el["language"] == "pl")["title"] }}
+    </span>
+    <span>{{ collect($row["additional"])->first(fn ($el) => $el["item"] == "color_product")["value"] }}</span>
+    <b>{{ $row["quantity"] }} szt.</b>
+    <span>{{ processFutureDelivery($row["future_delivery"]) }}</span>
     @empty
-    <div class="row">
-        <span class="ghost" style="grid-column: 1 / span 5">
-            Nie udało się znaleźć produktu o kodzie {{ $product_code }}
-        </span>
-    </div>
+    <span class="ghost" style="grid-column: 1 / span 5">
+        Nie udało się znaleźć produktu o kodzie {{ $product_code }}
+    </span>
     @endforelse
 </div>
 
