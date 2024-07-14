751226b252ae9974913d0238981c4467eb7a9860
Wojciech Przybyła <contact@wpww.pl>
2024-07-14 20:40:21 +0200

    🚧 footer basics added

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,10 @@
-<footer>
+<footer class="flex-right">
     <h2>{{ config("app.name") }}</h2>
     @auth
     Zalogowano jako {{ Auth::user()->name }}
     <a href="{{ route("logout") }}">Wyloguj</a>
     @endauth
+
+    <span class="ghost">
+    </span>
 </footer>
