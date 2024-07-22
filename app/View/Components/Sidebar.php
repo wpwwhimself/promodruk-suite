bea5a2a97d28f778a10d5c8172ec0740f7af45c6
Wojciech Przybyła <contact@wpww.pl>
2024-07-22 23:05:46 +0200

    💄 categories | sidebar

---
+++ app/View/Components/Sidebar.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Models\Category;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\View\Component;
+
+class Sidebar extends Component
+{
+    public Collection $categories;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        $this->categories = Category::orderBy("ordering")->get();
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.sidebar');
+    }
+}
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,3 +1,17 @@
 <aside>
     <h2>Kategorie</h2>
+
+    <ul>
+    @foreach ($categories as $category)
+        <a class="animatable"
+        @if ($category->external_link)
+            href="{{ $category->external_link }}" _target="blank"
+        @else
+            href=""
+        @endif
+        >
+            {{ $category->name }}
+        </a>
+    @endforeach
+    </ul>
 </aside>
