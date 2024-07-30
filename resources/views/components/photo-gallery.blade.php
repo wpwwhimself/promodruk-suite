d0999a2cd7b40751fb6bd311e15d4f5b55d470a1
Wojciech Przybyła <contact@wpww.pl>
2024-07-30 17:59:05 +0200

    💄 product | interactive photos

--- resources/views/components/photo-gallery.blade.php
+++ resources/views/components/photo-gallery.blade.php
@@ -4,17 +4,20 @@
 
 <div class="photo-gallery">
 
-<img src="{{ $images[0] }}" id="main-photo">
+<img src="{{ $images[0] }}" id="main-photo" onclick="openPhoto(event.target.src)">
 <div class="list flex-right wrap">
     @foreach ($images as $img)
-    <img onclick="highlightPhoto(this)" src="{{ $img }}" />
+    <img onclick="switchPhoto(this)" src="{{ $img }}" />
     @endforeach
 </div>
 
 </div>
 
 <script>
-highlightPhoto = (img) => {
+openPhoto = (url) => {
+    window.open(url, "_blank")
+}
+switchPhoto = (img) => {
     document.getElementById("main-photo").src = img.src
 }
 </script>
