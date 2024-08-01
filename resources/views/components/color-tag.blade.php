a6000c1a87770a1dc57d31cff7fdff0435480e75
Wojciech Przybyła <contact@wpww.pl>
2024-08-01 20:49:51 +0200

    🚧 main-attribute | building variant selector

---
+++ resources/views/components/color-tag.blade.php
@@ -0,0 +1,15 @@
+@props([
+    "color",
+    "link" => null,
+    "active" => false,
+])
+
+@if ($link)
+<a href="{{ $link }}">
+@endif
+
+<div {{ $attributes->class(["color-tag", "active" => $active]) }} style="--tile-color: {{ $color }}" {{ $attributes }}></div>
+
+@if ($link)
+</a>
+@endif
