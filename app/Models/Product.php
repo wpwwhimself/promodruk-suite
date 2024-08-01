8847ebd70850ee5ac1d1767dd7c0278e739d27d3
Wojciech Przybyła <contact@wpww.pl>
2024-08-01 19:20:47 +0200

    🗃️ main-attribute | added migrations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -20,6 +20,7 @@ class Product extends Model
         "visible",
         "name",
         "description",
+        "main_attribute_id",
         "extra_description",
         "images",
         "attributes",
