b58324d98235e4296b768e8d57757161dfab104b
Wojciech Przybyła <contact@wpww.pl>
2024-08-07 23:44:57 +0200

    ✨ double-colors | color input modes

--- app/Models/MainAttribute.php
+++ app/Models/MainAttribute.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class MainAttribute extends Model
 {
@@ -14,4 +15,21 @@ class MainAttribute extends Model
         "color",
         "description",
     ];
+
+    public const COLOR_MODES = [
+        "brak" => "none",
+        "pojedynczy" => "single",
+        "podwójny" => "double",
+        "wiele" => "multi",
+    ];
+
+    public function getColorModeAttribute()
+    {
+        return $this->color == "" ? self::COLOR_MODES["brak"] : (
+            $this->color == "multi" ? self::COLOR_MODES["wiele"] : (
+            Str::substrCount($this->color, "#") == 1 ? self::COLOR_MODES["pojedynczy"] : (
+            Str::substrCount($this->color, "#") == 2 ? self::COLOR_MODES["podwójny"] :
+            null
+        )));
+    }
 }
