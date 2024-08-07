f41cbab0d6521a1d0a52e79410600f6e88846dd3
Wojciech Przybyła <contact@wpww.pl>
2024-08-08 00:34:07 +0200

    💄 color tags - improved styling and handled double colors

--- resources/views/components/color-tag.blade.php
+++ resources/views/components/color-tag.blade.php
@@ -8,7 +8,24 @@
 
 <div {{ $attributes->class(["color-tag", "active" => $active, "no-color" => $color->get("color") == null]) }}
     title="{{ $color->get("name") }}"
+    @if ($color->get("color") == "multi")
+    style="background: linear-gradient(in hsl longer hue to bottom right, red 0 0)"
+@elseif (Str::contains($color->get("color"), ";"))
+    style="
+        --w-col-1: {{ Str::before($color->get("color"), ";") }};
+        --w-col-2: {{ Str::after($color->get("color"), ";") }};
+        background: linear-gradient(to bottom right, var(--w-col-1), var(--w-col-1) 50%, var(--w-col-2) 50%);
+    "
+@elseif ($color->get("color"))
     style="--tile-color: {{ $color->get("color") }}"
+@else
+    style="
+        --space: 15%;
+        --w-col-1: gold;
+        --w-col-2: #222;
+        background: repeating-linear-gradient(to bottom right, var(--w-col-1), var(--w-col-1) var(--space), var(--w-col-2) var(--space), var(--w-col-2) calc(var(--space) * 2));
+    "
+@endif
 >
 </div>
 
