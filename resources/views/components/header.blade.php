e677b3afcba185832695dbab493622ffcd27fa24
Wojciech Przybyła <contact@wpww.pl>
2024-08-08 19:39:52 +0200

    💄 layout-changes | search bar lifting applied

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,15 +3,16 @@
         <x-logo />
 
         <x-button action="none" label="Wszystkie produkty" icon="box" onclick="toggleCategoryDropdown(this)" />
-
-        <search>
-            <form action="{{ route('search') }}" method="post" class="flex-right middle">
-                @csrf
-                <label for="query">@svg("ik-search")</label>
-                <input id="query" type="text" placeholder="Wyszukaj..." name="query" />
-            </form>
-        </search>
     </div>
+
+    <search>
+        <form action="{{ route('search') }}" method="post" class="flex-right middle">
+            @csrf
+            <input id="query" type="text" placeholder="Wyszukaj produkty..." name="query" />
+            <x-button action="submit" label="" icon="search" />
+        </form>
+    </search>
+
     <div class="flex-right">
         <x-button :action="route('cart')" :label="count(session('cart', [])) ?: ''" icon="cart" />
     </div>
