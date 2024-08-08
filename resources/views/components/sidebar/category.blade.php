7420d199b16cfc3857229922724543dd5f4316ba
Wojciech Przybyła <contact@wpww.pl>
2024-08-08 21:04:20 +0200

    💄 sidebar | improved highlighting categories

--- resources/views/components/sidebar/category.blade.php
+++ resources/views/components/sidebar/category.blade.php
@@ -2,7 +2,12 @@
     "category"
 ])
 
-<a {{ $attributes->class(["animatable", "accent" => Route::currentRouteName() =="category-".$category->id]) }}
+<a {{ $attributes->class([
+    "animatable",
+    "active" => Route::currentRouteName() =="category-".$category->id,
+    "accent" => $category->children->count() && $category->allChildren->map(fn($cat) => "category-$cat->id")->contains(Route::currentRouteName()),
+    "bold" => $category->depth == 0,
+]) }}
 @if ($category->external_link)
     href="{{ $category->external_link }}" _target="blank"
 @else
