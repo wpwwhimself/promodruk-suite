bb6516749da795c86775b456a06fa9d8164b3bc4
Wojciech Przybyła <contact@wpww.pl>
2024-08-13 15:51:50 +0200

    🚸 dropdown | dismissing dropdown

--- resources/views/components/top-nav.blade.php
+++ resources/views/components/top-nav.blade.php
@@ -5,7 +5,7 @@
 
 <nav id="top-nav" class="flex-right">
     @if ($withAllProducts)
-    <x-button action="none" label="Wszystkie produkty" icon="hamburger" onclick="toggleCategoryDropdown(this)" class="all-products-btn" />
+    <x-button action="none" label="Wszystkie produkty" icon="hamburger" onclick="toggleCategoryDropdown()" class="all-products-btn" />
     <x-category-dropdown />
     @endif
 
@@ -19,9 +19,20 @@
 </nav>
 
 <script>
-    const toggleCategoryDropdown = (btn) => {
+    const dropdown = document.getElementById("category-dropdown")
+    const btn = document.querySelector(".all-products-btn")
+
+    const toggleCategoryDropdown = () => {
         btn.classList.toggle("active")
-        document.getElementById("category-dropdown").classList.toggle("visible")
+        dropdown.classList.toggle("visible")
+    }
+    const hideCategoryDropdown = () => {
+        btn.classList.remove("active")
+        dropdown.classList.remove("visible")
+    }
+
+    window.onclick = (event) => {
+        if (!event.target.matches("#category-dropdown li, .all-products-btn")) hideCategoryDropdown()
     }
 </script>
 
