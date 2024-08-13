9c7ef1f39bf248d5053bfbf56047aadd27566d35
Wojciech Przybyła <contact@wpww.pl>
2024-08-13 16:37:56 +0200

    💄 fixed grid layout to be proportional

--- resources/views/components/tiling.blade.php
+++ resources/views/components/tiling.blade.php
@@ -5,7 +5,7 @@
 @if ($count == "auto")
 <div class="tiling flex-right center">
 @else
-<div class="tiling grid" style="grid-template-columns: repeat({{ $count }}, 1fr);">
+<div class="tiling grid" style="grid-template-columns: repeat({{ $count }}, {{ 100/$count }}%);">
 @endif
 
     {{ $slot }}
