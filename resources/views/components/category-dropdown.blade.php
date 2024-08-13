70f050a021c5a8a90f71e29bcfa4b392c826c2c3
Wojciech Przybyła <contact@wpww.pl>
2024-08-13 15:40:52 +0200

    🚸 dropdown | auto-open populated categories

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -2,7 +2,9 @@
     <div id="columns" class="flex-right">
         <ul data-level="1">
             @foreach ($categories->whereNull("parent_id") as $cat)
-            <li class="animatable" onclick="openCategory({{ $cat->id }}, 2)" data-id="{{ $cat->id }}">
+            <li class="animatable" data-id="{{ $cat->id }}"
+                {{ $cat->children->count() > 0 ? 'onmouseover' : 'onclick' }}="openCategory({{ $cat->id }}, 2)"
+            >
                 {{ $cat->name }}
                 @if ($cat->children->count() > 0) <x-ik-chevron-right class="show-more" /> @endif
             </li>
@@ -29,7 +31,9 @@ const openCategory = (cat_id, level) => {
 
     document.querySelector(`#category-dropdown #columns ul:nth-child(${level - 1})`)
         .after(fromHTML(`<ul data-level="${level}">
-            ${cat.children.map(ccat => `<li class="animatable" onclick="openCategory(${ccat.id}, ${level + 1})" data-id="${ccat.id}">
+            ${cat.children.map(ccat => `<li class="animatable" data-id="${ccat.id}"
+                ${ccat.children.length > 0 ? 'onmouseover' : 'onclick'}="openCategory(${ccat.id}, ${level + 1})"
+            >
                 ${ccat.name}
                 ${ccat.children.length > 0 ? `<x-ik-chevron-right class="show-more" />` : ''}
             </li>`).join("")}
