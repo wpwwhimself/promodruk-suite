0ad3a0b0f37fd912c1684c0d159f6855f5551879
Wojciech Przybyła <contact@wpww.pl>
2024-08-17 20:44:02 +0200

    🎨 stock display | more spacious and readable

--- resources/views/components/stock-display.blade.php
+++ resources/views/components/stock-display.blade.php
@@ -17,11 +17,21 @@ fetch(`{{ env('MAGAZYN_API_URL') }}stock/{{ $productId }}/1`)
 
         data.forEach(row => {
             document.querySelector(".stock-display")
-                .append(fromHTML(`<span ${row.id == "{{ $highlightId }}" ? 'class="accent"' : ''}>
-                    <a href="/produkty/${row.id}">${row.original_color_name}</a>:
-                    <b>${row.current_stock} szt.</b>,
-                    przewidywana dostawa: ${row.future_delivery_amount ? `${row.future_delivery_amount} szt., ${row.future_delivery_date}` : "brak"}
+                .append(fromHTML(`<span class="grid ${row.id == "{{ $highlightId }}" ? 'accent' : ''}" style="grid-template-columns: repeat(2, auto);">
+                    <span>
+                        Kolor: <a href="/produkty/${row.id}">${row.original_color_name}</a>:
+                        <b>${row.current_stock} szt.</b>,
+                    </span>
+                    <span>
+                        Przewid. dost.: ${row.future_delivery_amount ? `${row.future_delivery_amount} szt., ${row.future_delivery_date}` : "brak"}
+                    </span>
                 </span>`))
         })
     })
 </script>
+
+<style>
+.stock-display {
+    margin-block: 0.5em;
+}
+</style>
