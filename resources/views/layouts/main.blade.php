676aabdb9378f9844c57a4fce03568b90c50c4a5
Wojciech Przybyła <contact@wpww.pl>
2024-08-17 19:37:03 +0200

    🎨 listing | pushed title to the top

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,19 +14,19 @@
             <x-sidebar />
             <main>
 
-            @if (
-                getSetting("welcome_text_visible") == 2
-                || getSetting("welcome_text_visible") == 1 && Route::currentRouteName() == "home"
-            )
-            {!! \Illuminate\Mail\Markdown::parse(getSetting("welcome_text_content")) !!}
-            @endif
-
             @yield("before-title")
 
             <h1>
                 @yield("title")
                 <small class="ghost">@yield("subtitle")</small>
             </h1>
+
+            @if (
+                getSetting("welcome_text_visible") == 2
+                || getSetting("welcome_text_visible") == 1 && Route::currentRouteName() == "home"
+            )
+            {!! \Illuminate\Mail\Markdown::parse(getSetting("welcome_text_content")) !!}
+            @endif
 
             @yield("interactives")
 
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,5 @@
 @extends("layouts.main")
-@section("title", "Katalog produktów")
+@section("title", "Kategorie główne")
 
 @section("content")
 
