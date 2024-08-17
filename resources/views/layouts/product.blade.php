a31a424ac64cb617e740be331fbc7f992bcfe6bc
Wojciech Przybyła <contact@wpww.pl>
2024-08-17 18:57:02 +0200

    💄 reframed listings

---
+++ resources/views/layouts/product.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<x-layout.head />
+<body class="flex-down center">
+    <div id="header-wrapper" class="flex-down animatable">
+        <x-header />
+        <x-top-nav
+            :pages="\App\Models\TopNavPage::ordered()->get()->map(fn ($page) => [$page->name, $page->slug])"
+            with-all-products
+        />
+    </div>
+    <div id="main-wrapper" class="flex-down">
+        <div id="sidebar-wrapper" class="grid">
+            <x-sidebar />
+            <div>
+                @if (
+                    getSetting("welcome_text_visible") == 2
+                    || getSetting("welcome_text_visible") == 1 && Route::currentRouteName() == "home"
+                )
+                {!! \Illuminate\Mail\Markdown::parse(getSetting("welcome_text_content")) !!}
+                @endif
+
+                @yield("before-title")
+
+                <main class="framed">
+                    <div class="grid" style="grid-template-columns: repeat(2, 50%);">
+                        <div class="flex-down">
+                            @yield("left-side")
+                        </div>
+
+                        <div id="content">
+                            <h1>
+                                @yield("title")
+                                <small class="ghost">@yield("subtitle")</small>
+                            </h1>
+                            @yield("content")
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+    </div>
+    <x-footer />
+
+    @foreach (["success", "error"] as $status)
+    @if (session($status))
+    <x-popup-alert :status="$status" />
+    @endif
+    @endforeach
+
+    @bukScripts(true)
+</body>
+</html>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,4 +1,4 @@
-@extends("layouts.main")
+@extends("layouts.product")
 @section("title", $product->name)
 @section("subtitle", $product->id)
 
@@ -6,62 +6,58 @@
 <x-breadcrumbs :category="$product->categories" :product="$product" />
 @endsection
 
+@section("left-side")
+<x-photo-gallery :images="$product->images" :thumbnails="$product->thumbnails" />
+@endsection
+
 @section("content")
 
-<div class="grid" style="grid-template-columns: repeat(2, 50%);">
-    <div class="flex-down">
-        <x-photo-gallery :images="$product->images" :thumbnails="$product->thumbnails" />
-    </div>
+<h2>
+    <small class="ghost">Cena netto (bez znakowania):</small>
+    {{ asPln($product->price) }}
+</h2>
 
-    <div>
-        <h2>
-            <small class="ghost">Cena netto (bez znakowania):</small>
-            {{ asPln($product->price) }}
-        </h2>
+@if ($product->family->count() > 1)
+<h3>Wybierz kolor, aby zobaczyć zdjęcia i sprawdzić stan magazynowy</h3>
 
-        @if ($product->family->count() > 1)
-        <h3>Wybierz kolor, aby zobaczyć zdjęcia i sprawdzić stan magazynowy</h3>
-
-        <div class="flex-right wrap">
-            @foreach ($product->family as $alt)
-            <x-color-tag :color="collect($alt->color)"
-                :active="$alt->id == $product->id"
-                :link="route('product', ['id' => $alt->id])"
-            />
-            @endforeach
-        </div>
-        @endif
-
-        <x-stock-display :product-id="$product->id" :long="true" />
-
-        <h3>Opis</h3>
-        <div>{{ \Illuminate\Mail\Markdown::parse($product->description ?? "") }}</div>
-        <div>{{ \Illuminate\Mail\Markdown::parse($product->extra_description ?? "") }}</div>
-
-        <form action="{{ route('add-to-cart') }}" method="post" enctype="multipart/form-data">
-            @csrf
-
-            <h3>Parametry zapytania</h3>
-
-            <input type="hidden" name="product_id" value="{{ $product->id }}">
-
-            @foreach ($product->attributes as $attr)
-            <x-multi-input-field
-                :name="'attr-'.$attr['id']" :label="$attr['name']"
-                :options="collect($attr['variants'])->flatMap(fn($var) => [$var['name'] => $var['id']])"
-            />
-            @endforeach
-
-            <x-input-field type="TEXT" label="Planowane ilości do wyceny" placeholder="100/200/300 lub żółty - 100 szt., zielony - 50 szt. itp." name="amount" rows="2" />
-            <x-input-field type="TEXT" label="Komentarz do zapytania" placeholder="np. dotyczące projektu..." name="comment" />
-            <x-input-field type="file" label="Pliki projektu" name="files[]" multiple />
-
-            <div class="flex-right center">
-                <x-button action="submit" label="Dodaj do zapytania" icon="cart" />
-                @auth <x-button action="{{ route('products-edit', ['id' => $product->id]) }}" label="Edytuj produkt" icon="edit" /> @endauth
-            </div>
-        </form>
-    </div>
+<div class="flex-right wrap">
+    @foreach ($product->family as $alt)
+    <x-color-tag :color="collect($alt->color)"
+        :active="$alt->id == $product->id"
+        :link="route('product', ['id' => $alt->id])"
+    />
+    @endforeach
 </div>
+@endif
+
+<x-stock-display :product-id="$product->id" :long="true" />
+
+<h3>Opis</h3>
+<div>{{ \Illuminate\Mail\Markdown::parse($product->description ?? "") }}</div>
+<div>{{ \Illuminate\Mail\Markdown::parse($product->extra_description ?? "") }}</div>
+
+<form action="{{ route('add-to-cart') }}" method="post" enctype="multipart/form-data">
+    @csrf
+
+    <h3>Parametry zapytania</h3>
+
+    <input type="hidden" name="product_id" value="{{ $product->id }}">
+
+    @foreach ($product->attributes as $attr)
+    <x-multi-input-field
+        :name="'attr-'.$attr['id']" :label="$attr['name']"
+        :options="collect($attr['variants'])->flatMap(fn($var) => [$var['name'] => $var['id']])"
+    />
+    @endforeach
+
+    <x-input-field type="TEXT" label="Planowane ilości do wyceny" placeholder="100/200/300 lub żółty - 100 szt., zielony - 50 szt. itp." name="amount" rows="2" />
+    <x-input-field type="TEXT" label="Komentarz do zapytania" placeholder="np. dotyczące projektu..." name="comment" />
+    <x-input-field type="file" label="Pliki projektu" name="files[]" multiple />
+
+    <div class="flex-right center">
+        <x-button action="submit" label="Dodaj do zapytania" icon="cart" />
+        @auth <x-button action="{{ route('products-edit', ['id' => $product->id]) }}" label="Edytuj produkt" icon="edit" /> @endauth
+    </div>
+</form>
 
 @endsection
