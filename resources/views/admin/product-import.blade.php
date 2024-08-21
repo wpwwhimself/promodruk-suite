85835781179486fce4f603c99f21155023d301ca
Wojciech Przybyła <contact@wpww.pl>
2024-08-21 22:50:28 +0200

    🚸 import | expand photos on fetch list

--- resources/views/admin/product-import.blade.php
+++ resources/views/admin/product-import.blade.php
@@ -54,7 +54,9 @@
                     <tr>
                         <td>{{ $product["id"] }}</td>
                         <td>
-                            <img src="{{ collect($product["thumbnails"])->first() }}" alt="{{ $product["name"] }}" class="inline">
+                            <img src="{{ collect($product["thumbnails"])->first() }}" alt="{{ $product["name"] }}" class="inline"
+                                {{ Popper::pop("<img src='" . collect($product["thumbnails"])->first() . "' />") }}
+                            >
                             {{ $product["name"] }}
                         </td>
                         <td><input type="checkbox" name="ids[]" value="{{ $product["id"] }}" /></td>
