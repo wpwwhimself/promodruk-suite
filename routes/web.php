b36cebe94181138b170847c49282a3de11a99e74
Wojciech Przybyła <contact@wpww.pl>
2024-08-22 22:25:57 +0200

    🚑 fixed allowing product codes to have slashes

--- routes/web.php
+++ routes/web.php
@@ -49,9 +49,9 @@ Route::controller(ProductController::class)->prefix("produkty")->group(function 
     }
     Route::get("kategoria/{id}", fn(int $id) => redirect(route("category-$id")));
 
-    Route::get("{id?}", "listProduct")->name("product");
     Route::get("szukaj/{query?}", "listSearchResults")->name("search-results");
     Route::post("szukaj", fn(Request $rq) => redirect()->route("search-results", ["query" => $rq->input("query")]))->name("search");
+    Route::get("{id?}", "listProduct")->name("product")->where("id", ".*");
 });
 
 Route::controller(ShoppingCartController::class)->prefix("koszyk")->group(function () {
