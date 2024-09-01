07768010e2ae013d5024357471941f88521612aa
Wojciech Przybyła <contact@wpww.pl>
2024-09-01 22:24:13 +0200

    🩹 showcase | more sleek

--- resources/views/components/showcase.blade.php
+++ resources/views/components/showcase.blade.php
@@ -1,8 +1,7 @@
 <div id="showcase" class="flex-down center rounded max-width-wrapper" onmouseenter="clickOnEmbed()">
-    <h2 class="animatable">{{ getSetting("showcase_top_heading") }}</h2>
-    <div class="flex-right center-both">
+    <div class="flex-right center">
         @if (getSetting("showcase_side_text"))
-        <div>{!! getSetting("showcase_side_text") !!}</div>
+        <div class="framed rounded" style="align-content: center;">{!! getSetting("showcase_side_text") !!}</div>
         @endif
 
         <video src="{{ asset("storage/meta/showcase.mp4") }}"
