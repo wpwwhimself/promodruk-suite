7fa24df6d0f50778c33bef57fbb94adc29c9e4b4
Wojciech Przybyła <contact@wpww.pl>
2024-09-03 09:54:54 +0200

    💄 showcase | removed frame

--- resources/views/components/showcase.blade.php
+++ resources/views/components/showcase.blade.php
@@ -1,7 +1,7 @@
 <div id="showcase" class="flex-down center rounded max-width-wrapper" onmouseenter="clickOnEmbed()">
     <div class="flex-right center">
         @if (getSetting("showcase_side_text"))
-        <div class="framed rounded" style="align-content: center;">{!! getSetting("showcase_side_text") !!}</div>
+        <div style="align-content: center;">{!! getSetting("showcase_side_text") !!}</div>
         @endif
 
         <video src="{{ asset("storage/meta/showcase.mp4") }}"
