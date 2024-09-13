f2bf60b791b23114270aa7c457b58ee4e194c114
Wojciech Przybyła <contact@wpww.pl>
2024-09-13 19:04:54 +0200

    🚸 attributes | added filters for empty and filled colors

--- resources/views/admin/attributes.blade.php
+++ resources/views/admin/attributes.blade.php
@@ -9,36 +9,40 @@
 <a href="{{ route("main-attributes-prune") }}">Usuń nieużywane cechy podstawowe</a>
 @endif
 
-@if ($mainAttributes->where("color", "")->count() > 0)
-<h3 class="danger">Nieopisane</h3>
+<div class="flex-right">
+    @if (!empty(request("show")))
+        <a href="{{ route("attributes") }}">Pokaż wszystkie</a>
+    @endif
+    @if (request("show") != "missing")
+        <a href="{{ route("attributes", ["show" => "missing"]) }}">Pokaż nieopisane</a>
+    @endif
+    @if (request("show") != "filled")
+        <a href="{{ route("attributes", ["show" => "filled"]) }}">Pokaż opisane</a>
+    @endif
+</div>
+
 <ul>
-    @foreach ($mainAttributes->where("color", "") as $attribute)
+    @php
+        $data = (request("show") == "missing")
+            ? $mainAttributes->where("color", "")
+            : (request("show") == "filled"
+                ? $mainAttributes->where("color", "!=", "")
+                : $mainAttributes
+            )
+    @endphp
+    @forelse ($data as $attribute)
     <li>
         <a href="{{ route("main-attributes-edit", $attribute->id) }}">
             <x-color-tag :color="$attribute" />
             {{ $attribute->name }}
         </a>
 
-        @if (isset($productExamples[$attribute->name]))
+        @if (isset($productExamples[$attribute->name]) && $attribute->color == "")
         <small class="ghost">(w produktach: {{ $productExamples[$attribute->name]->pluck("id")->join("; ") }})</small>
         @endif
     </li>
-    @endforeach
-</ul>
-
-<h3>Pełna lista</h3>
-@endif
-
-<ul>
-    @forelse ($mainAttributes as $attribute)
-    <li>
-        <a href="{{ route("main-attributes-edit", $attribute->id) }}">
-            <x-color-tag :color="$attribute" />
-            {{ $attribute->name }}
-        </a>
-    </li>
     @empty
-    <li class="ghost">Brak zdefiniowanych cech podstawowych</li>
+    <li class="ghost">Brak {{ empty(request("show")) ? "zdefiniowanych" : "" }} cech podstawowych</li>
     @endforelse
 </ul>
 
