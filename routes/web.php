6d859455b2541dd63382522687d891e627b69860
Wojciech Przybyła <contact@wpww.pl>
2024-09-14 23:52:43 +0200

    🔐 unified auth with other apps

--- routes/web.php
+++ routes/web.php
@@ -26,33 +26,32 @@ Route::controller(AuthController::class)->prefix("auth")->group(function () {
 });
 
 Route::middleware("auth")->controller(AdminController::class)->prefix("admin")->group(function () {
-    foreach(AdminController::$pages as [$label, $route]) {
-        Route::get(Str::slug($route), Str::camel($route))->name(Str::kebab($route));
+    foreach(AdminController::$pages as [$label, $route, $role]) {
+        $role
+            ? Route::middleware("role:$role")->get(Str::slug($route), Str::camel($route))->name(Str::kebab($route))
+            : Route::get(Str::slug($route), Str::camel($route))->name(Str::kebab($route));
 
         if ($route !== "dashboard") {
             Route::get($route."/edit/{id?}", Str::singular(Str::camel($route))."Edit")->name("$route-edit");
         }
     }
-    Route::get("main-attributes/edit/{id?}", "mainAttributeEdit")->name("main-attributes-edit");
-    Route::get("main-attributes/prune", "mainAttributePrune")->name("main-attributes-prune");
 
-    Route::prefix("products/import")->group(function () {
-        Route::get("/", "productImport")->name("products-import");
-        Route::post("/", "productImportFetch")->name("products-import-fetch");
-        Route::post("/choose", "productImportChoose")->name("products-import-choose");
+    Route::middleware("role:Edytor")->prefix("main-attributes")->group(function () {
+        Route::get("/edit/{id?}", "mainAttributeEdit")->name("main-attributes-edit");
+        Route::get("/prune", "mainAttributePrune")->name("main-attributes-prune");
+
+        Route::prefix("settings/update")->group(function () {
+            foreach(AdminController::$updaters as $slug) {
+                Route::post(Str::slug($slug), Str::camel("update-".$slug))->name(Str::kebab("update-".$slug));
+            }
+        });
     });
 
-    Route::prefix("synchronizations")->group(function () {
+
+    Route::middleware("role:Administrator")->prefix("synchronizations")->group(function () {
         Route::get("enable/{supplier_name}/{mode}/{enabled}", "synchEnable")->name("synch-enable");
         Route::get("reset/{supplier_name?}", "synchReset")->name("synch-reset");
     });
-
-    Route::prefix("settings/update")->group(function () {
-        foreach(AdminController::$updaters as $slug) {
-            Route::post(Str::slug($slug), Str::camel("update-".$slug))->name(Str::kebab("update-".$slug));
-        }
-    });
-
 });
 
 Route::prefix("test")->group(function () {
