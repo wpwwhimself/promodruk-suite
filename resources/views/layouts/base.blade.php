8e17f5d7c69635c9f645b42d5af5dbd7b62552fc
Wojciech Przybyła <contact@wpww.pl>
2024-09-16 20:57:31 +0200

    👔 removed sellout info

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -51,9 +51,7 @@
                 </span>
 
                 <span>
-                    <span>
-                        Projekt i wykonanie: <a href="https://wpww.pl/">Wojciech Przybyła</a>
-                    </span>
+                    {{-- <span>Projekt i wykonanie: <a href="https://wpww.pl/">Wojciech Przybyła</a></span> --}}
                     @if (Auth::check() && userIs("technical"))
                     <a href="https://github.com/wpwwhimself/promodruk-kwazar/tree/main/docs">Dokumentacja</a>
                     @endif
