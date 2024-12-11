a046fd8e6a48d91af6a8ac79ac66d36a89189ea0
Wojciech Przybyła <contact@wpww.pl>
2024-12-11 21:56:12 +0100

    📱 mobile | put subpages into separate drawer

--- resources/views/components/top-nav.blade.php
+++ resources/views/components/top-nav.blade.php
@@ -4,41 +4,60 @@
 ])
 
 <nav id="top-nav">
-    <div class="max-width-wrapper flex-right">
-        @if ($withAllProducts)
-        <x-button action="none" label="Strona główna" icon="home-alt" class="home-btn" onclick="window.location.href = '{{ route('home') }}';" />
-        <x-button action="none" label="Wszystkie produkty" icon="hamburger"
-            onmouseenter="toggleCategoryDropdown('add')"
-            onmouseleave="toggleCategoryDropdown('remove')"
-            onclick="toggleCategoryDropdown()"
-            class="all-products-btn"
-        >
-            <x-category-dropdown />
-        </x-button>
-        @endif
+    <div class="max-width-wrapper flex-right but-mobile-down">
+        <div data-role="main-buttons" class="flex-right">
+            @if ($withAllProducts)
+            <x-button action="none" label="Strona główna" icon="home-alt" class="home-btn" onclick="window.location.href = '{{ route('home') }}';" />
+            <x-button action="none" label="Wszystkie produkty" icon="hamburger"
+                onmouseenter="toggleCategoryDropdown('add')"
+                onmouseleave="toggleCategoryDropdown('remove')"
+                onclick="toggleCategoryDropdown()"
+                class="all-products-btn"
+            >
+                <x-category-dropdown />
+            </x-button>
+            @endif
 
-        @foreach ($pages as [$label, $route])
-        <a href="{{ route($route) }}"
-            class="{{ Route::currentRouteName() == $route ? "active" : "" }} padded animatable flex-right middle"
-        >
-            {{ $label }}
-        </a>
-        @endforeach
+            {{-- mobile menu switch --}}
+            <x-button action="none" label="Informacje" icon="info-circle" icon-set="iconoir"
+                class="hidden but-mobile-show" data-role="subpages-toggle"
+            />
+        </div>
+
+        <div data-role="subpages" class="flex-right but-mobile-hide wrap">
+            @foreach ($pages as [$label, $route])
+            <a href="{{ route($route) }}"
+                class="{{ Route::currentRouteName() == $route ? "active" : "" }} padded animatable flex-right middle"
+            >
+                {{ $label }}
+            </a>
+            @endforeach
+        </div>
     </div>
 </nav>
 
 <script>
-    const dropdown = document.getElementById("category-dropdown")
-    const btn = document.querySelector(".all-products-btn")
+//? dropdown handling ?//
+const dropdown = document.getElementById("category-dropdown")
+const btn = document.querySelector(".all-products-btn")
 
-    const toggleCategoryDropdown = (method = "toggle") => {
-        btn.classList[method]("active")
-        dropdown.classList[method]("visible")
-    }
+const toggleCategoryDropdown = (method = "toggle") => {
+    btn.classList[method]("active")
+    dropdown.classList[method]("visible")
+}
 
-    window.onclick = (event) => {
-        if (!event.target.matches("#category-dropdown li, .all-products-btn")) toggleCategoryDropdown('remove')
-    }
+window.onclick = (event) => {
+    if (!event.target.matches("#category-dropdown li, .all-products-btn")) toggleCategoryDropdown('remove')
+}
+
+//? mobile menu switch ?//
+const subpagesToggle = document.querySelector("[data-role=subpages-toggle]")
+const subpages = document.querySelector("[data-role=subpages]")
+
+subpagesToggle.addEventListener("click", () => {
+    subpagesToggle.classList.toggle("active")
+    subpages.classList.toggle("but-mobile-hide")
+})
 </script>
 
 <style>
