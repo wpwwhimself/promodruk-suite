18594be3316fb1ea9f5a467fab1996bb42a52d20
Wojciech Przybyła <contact@wpww.pl>
2024-12-11 22:08:46 +0100

    📱 mobile | revised dropdown

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -11,23 +11,28 @@ const openCategory = (cat_id, level) => {
 
     if (level != 1)
     {
+        target = document.querySelector(`#category-dropdown li[data-id="${cat_id}"]`)
+        children = cat.children
+
+        if (target.classList.contains("active")) {
+            hideCategory(cat_id)
+            return
+        }
+
         document.querySelectorAll(`#category-dropdown ul`)?.forEach(ul => {
             if (ul.dataset.level >= level) ul.remove()
         })
         document.querySelectorAll(`#category-dropdown ul[data-level="${level - 1}"] li.active`)?.forEach(li => {
             li.classList.remove("active")
         })
-
-        target = document.querySelector(`#category-dropdown li[data-id="${cat_id}"]`)
-        target.classList.add("active")
-
-        children = cat.children
     }
     else
     {
         target = document.querySelector(`#category-dropdown`)
         children = categories.filter(cat => cat.parent_id == null)
     }
+
+    target.classList.add("active")
 
     target.append(fromHTML(`<ul data-level="${level}">
         ${children.map(ccat => `<li class="animatable" data-id="${ccat.id}"
@@ -39,12 +44,22 @@ const openCategory = (cat_id, level) => {
             ${ccat.children.length > 0 ? `<x-ik-chevron-right class="show-more" />` : ''}
         </li>`).join("")}
     </ul>`))
+
+    // hide other categories on mobile
+    if (target != document.querySelector(`#category-dropdown`)) {
+        Array.from(target.parentElement.children)
+            .filter(li => !li.classList.contains("active"))
+            .forEach(li => li.classList.add("but-mobile-hide"))
+    }
 }
 
 openCategory(null, 1)
 
 const hideCategory = (cat_id) => {
-    document.querySelector(`#category-dropdown li[data-id="${cat_id}"] ul`)?.remove()
-    document.querySelector(`#category-dropdown li[data-id="${cat_id}"]`).classList.remove("active")
+    const target = document.querySelector(`#category-dropdown li[data-id="${cat_id}"]`)
+    target.nextSibling.querySelector(`ul`)?.remove()
+    target.classList.remove("active")
+    Array.from(target.parentElement.children)
+        .forEach(li => li.classList.remove("but-mobile-hide"))
 }
 </script>
