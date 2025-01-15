8c7a5aa2fdc7556ddf341c0d4e7f59f212ced181
Wojciech Przybyła <contact@wpww.pl>
2025-01-15 22:02:10 +0100

    🚑 this ain't working

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -11,11 +11,9 @@
         @yield("description")
     </p>
 
-    @if (userIs("technical"))
     <p class="ghost">
         {{ $exception->getMessage() }}
     </p>
-    @endif
 </section>
 
 @endsection
