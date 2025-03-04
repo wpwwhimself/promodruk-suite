1d0e87608f7ec0382eace869915e99993243038e
Wojciech Przybyła <contact@wpww.pl>
2025-03-04 13:52:02 +0100

    🚑 easygifts | updated import id recognition

--- app/DataIntegrators/EasygiftsHandler.php
+++ app/DataIntegrators/EasygiftsHandler.php
@@ -51,9 +51,9 @@ class EasygiftsHandler extends ApiHandler
         $imported_ids = [];
 
         foreach ($products as $product) {
-            $imported_ids[] = (int) $product->baseinfo->{self::SKU_KEY};
+            $imported_ids[] = (int) $product->baseinfo->{self::PRIMARY_KEY};
 
-            if ($this->sync->current_external_id != null && $this->sync->current_external_id > intval($product->baseinfo->{self::SKU_KEY})) {
+            if ($this->sync->current_external_id != null && $this->sync->current_external_id > intval($product->baseinfo->{self::PRIMARY_KEY})) {
                 $counter++;
                 continue;
             }
@@ -181,7 +181,7 @@ class EasygiftsHandler extends ApiHandler
 
         $this->saveProduct(
             $product->baseinfo->{self::SKU_KEY},
-            $product->baseinfo->{self::SKU_KEY},
+            $product->baseinfo->{self::PRIMARY_KEY},
             $product->baseinfo->name,
             $product->baseinfo->intro,
             $this->getPrefixedId($product->baseinfo->code_short),
