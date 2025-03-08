22958306b9c45c46c2c2e7435d6214d09a6eaf3c
Wojciech Przybyła <contact@wpww.pl>
2025-03-08 12:28:32 +0100

    ✨ [color-dictionary] added triple color tile

--- resources/views/components/color-tag.blade.php
+++ resources/views/components/color-tag.blade.php
@@ -10,11 +10,20 @@
 <div {{ $attributes->class(["color-tile", "active" => $active, "no-color" => $color->get("color") == null]) }}
     @if ($color->get("color") == "multi")
     style="background: linear-gradient(in hsl longer hue to bottom right, red 0 0)"
-@elseif (Str::contains($color->get("color"), ";"))
+@elseif (Str::substrCount($color->get('color'), ";") > 0)
     style="
-        --w-col-1: {{ Str::before($color->get("color"), ";") }};
-        --w-col-2: {{ Str::after($color->get("color"), ";") }};
-        background: linear-gradient(to bottom right, var(--w-col-1), var(--w-col-1) 50%, var(--w-col-2) 50%);
+        --w-col-1: {{ Str::of($color->get('color'))->matchAll('/(#[0-9a-f]{6})/')[0] ?? '' }};
+        --w-col-2: {{ Str::of($color->get('color'))->matchAll('/(#[0-9a-f]{6})/')[1] ?? '' }};
+        --w-col-3: {{ Str::of($color->get('color'))->matchAll('/(#[0-9a-f]{6})/')[2] ?? '' }};
+        @switch (Str::substrCount($color->get('color'), ";"))
+            @case (2)
+            background: conic-gradient(var(--w-col-1) 33%, var(--w-col-2) 33%, var(--w-col-2) 67%, var(--w-col-3) 67%);
+            @break
+
+            @case (1)
+            background: linear-gradient(to bottom right, var(--w-col-1) 50%, var(--w-col-2) 50%);
+            @break
+        @endswitch
     "
 @elseif ($color->get("color"))
     style="--tile-color: {{ $color->get("color") }}"
