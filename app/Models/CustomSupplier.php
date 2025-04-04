78ae48de02ec8ba04bc84eefa03b0974d6b02975
Wojciech Przybyła <contact@wpww.pl>
2025-04-04 20:16:07 +0200

    🩹 suppliers | fixed displaying product count

--- app/Models/CustomSupplier.php
+++ app/Models/CustomSupplier.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -29,9 +31,11 @@ class CustomSupplier extends Model
     #endregion
 
     #region relations
-    public function productFamilies()
+    public function productFamilies(): Attribute // not really a relation, I know
     {
-        return $this->hasMany(ProductFamily::class, "source", "name");
+        return Attribute::make(
+            get: fn () => ProductFamily::where("source", ProductFamily::CUSTOM_PRODUCT_GIVEAWAY.$this->id)->get(),
+        );
     }
     #endregion
 }
