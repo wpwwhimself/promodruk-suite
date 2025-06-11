aceff1a0c9459dbffedb5ed51a70103c5d3c450d
Wojciech Przybyła <contact@wpww.pl>
2025-06-11 21:49:53 +0200

    🩹 axpol | fixed downloading markings

--- app/DataIntegrators/AxpolHandler.php
+++ app/DataIntegrators/AxpolHandler.php
@@ -262,7 +262,7 @@ class AxpolHandler extends ApiHandler
             "prices" => $prices,
         ] = $data;
 
-        $markings = $markings->filter(fn($p) => $p[self::PRIMARY_KEY] == $product[self::PRIMARY_KEY]) ?? [];
+        $markings = $markings->firstWhere(fn($p) => $p[self::PRIMARY_KEY] == $product[self::PRIMARY_KEY])["Print"] ?? [];
 
         foreach ($markings as $marking) {
             foreach ($marking["Technique"] ?? [] as $technique) {
