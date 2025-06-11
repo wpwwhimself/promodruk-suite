54a85abeea71f6d8f1742c685a9e8e28fcf4d02e
Wojciech Przybyła <contact@wpww.pl>
2025-06-11 22:21:00 +0200

    🩹 synch | fixed synchronization not going

--- app/Jobs/RefreshProductsJob.php
+++ app/Jobs/RefreshProductsJob.php
@@ -52,78 +52,84 @@ class RefreshProductsJob implements ShouldQueue
         $counter = 0;
         $total = count($products_starting);
 
-        foreach ($products_starting->chunk(500) as $i => $product_batch) {
-            $this->log("Starting batch $i");
-            [
-                "products" => $products,
-                "missing" => $missing,
-            ] = Http::post(env("MAGAZYN_API_URL") . "products/for-refresh", [
-                "ids" => $product_batch,
-                "families" => true,
-            ])->collect();
-
-            $status = $this->status([
-                "status" => "przetwarzanie",
-                "progress" => 0,
-            ]);
-
-            foreach (collect($products)->sortBy("id") as $family) {
-                $counter++;
-                if ($status["current_id"] > $family["id"]) continue;
+        try {
+            foreach ($products_starting->chunk(500) as $i => $product_batch) {
+                $this->log("Starting batch $i");
+                [
+                    "products" => $products,
+                    "missing" => $missing,
+                ] = Http::post(env("MAGAZYN_API_URL") . "products/for-refresh", [
+                    "ids" => $product_batch,
+                    "families" => true,
+                ])->collect();
 
                 $status = $this->status([
-                    "current_id" => $family["id"],
-                    "progress" => round($counter / $total * 100),
+                    "status" => "przetwarzanie",
+                    "progress" => 0,
                 ]);
 
-                $updated_ids = [];
-                foreach ($family["products"] as $product) {
-                    $product = Product::updateOrCreate(["id" => $product["id"]], [
-                        "product_family_id" => $product["product_family_id"],
-                        "front_id" => $product["front_id"],
-                        "name" => $product["name"],
-                        "description" => $product["combined_description"] ?? null,
-                        "description_label" => $product["product_family"]["description_label"],
-                        "images" => $product["combined_images"] ?? null,
-                        "thumbnails" => $product["combined_thumbnails"] ?? null,
-                        "color" => $product["color"],
-                        "sizes" => $product["sizes"],
-                        "extra_filtrables" => $product["extra_filtrables"],
-                        "brand_logo" => $product["brand_logo"],
-                        "original_sku" => $product["original_sku"],
-                        "price" => $product["price"],
-                        "tabs" => $product["combined_tabs"] ?? null,
+                foreach (collect($products)->sortBy("id") as $family) {
+                    $counter++;
+                    if ($status["current_id"] > $family["id"]) continue;
+
+                    $status = $this->status([
+                        "current_id" => $family["id"],
+                        "progress" => round($counter / $total * 100),
                     ]);
-                    $updated_ids[] = $product->id;
+
+                    $updated_ids = [];
+                    foreach ($family["products"] as $product) {
+                        $product = Product::updateOrCreate(["id" => $product["id"]], [
+                            "product_family_id" => $product["product_family_id"],
+                            "front_id" => $product["front_id"],
+                            "name" => $product["name"],
+                            "description" => $product["combined_description"] ?? null,
+                            "description_label" => $product["product_family"]["description_label"],
+                            "images" => $product["combined_images"] ?? null,
+                            "thumbnails" => $product["combined_thumbnails"] ?? null,
+                            "color" => $product["color"],
+                            "sizes" => $product["sizes"],
+                            "extra_filtrables" => $product["extra_filtrables"],
+                            "brand_logo" => $product["brand_logo"],
+                            "original_sku" => $product["original_sku"],
+                            "price" => $product["price"],
+                            "tabs" => $product["combined_tabs"] ?? null,
+                        ]);
+                        $updated_ids[] = $product->id;
+                    }
+
+                    // delete missing product variants
+                    Product::whereNotIn("id", $updated_ids)
+                        ->where("product_family_id", $family["id"])
+                        ->delete();
                 }
 
-                // delete missing product variants
-                Product::whereNotIn("id", $updated_ids)
-                    ->where("product_family_id", $family["id"])
-                    ->delete();
+                $missing_total = array_merge($missing_total, $missing);
             }
 
-            $missing_total = array_merge($missing_total, $missing);
+            $this->log("Refreshing products finished. Time to clean up...", "info", ["missing_count" => count($missing_total)]);
+            $status = $this->status([
+                "status" => "sprzątanie",
+            ]);
+
+            if (count($missing_total) > 0) {
+                Product::whereIn("product_family_id", $missing_total)->delete();
+            }
+
+            $status = $this->status([
+                "status" => "gotowe",
+                "current_id" => null,
+                "progress" => 100,
+                "last_sync_completed_at" => now(),
+                "last_sync_zero_to_full" => now()->diffInSeconds($status["last_sync_zero_at"]),
+            ]);
+
+            $this->log("Done!");
+        } catch (\Throwable $th) {
+            $status = $this->status([
+                "status" => "błąd",
+            ]);
         }
-
-        $this->log("Refreshing products finished. Time to clean up...", "info", ["missing_count" => count($missing_total)]);
-        $status = $this->status([
-            "status" => "sprzątanie",
-        ]);
-
-        if (count($missing_total) > 0) {
-            Product::whereIn("product_family_id", $missing_total)->delete();
-        }
-
-        $status = $this->status([
-            "status" => "gotowe",
-            "current_id" => null,
-            "progress" => 100,
-            "last_sync_completed_at" => now(),
-            "last_sync_zero_to_full" => now()->diffInSeconds($status["last_sync_zero_at"]),
-        ]);
-
-        $this->log("Done!");
     }
 
     private function log($message, $lvl = "info", $context = [])
