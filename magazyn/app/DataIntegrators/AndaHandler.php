b400df7c6ce1a3b43c97ae5c92dea171335a18de
Wojciech Przybyła <contact@wpww.pl>
2026-02-11 16:00:16 +0100

    🗄️🪗 anda - fixed for clothings

--- magazyn/app/DataIntegrators/AndaHandler.php
+++ magazyn/app/DataIntegrators/AndaHandler.php
@@ -258,7 +258,7 @@ class AndaHandler extends ApiHandler
                 "size_name" => Str::of($s->{self::SKU_KEY})->afterLast("_"),
                 "size_code" => Str::of($s->{self::SKU_KEY})->afterLast("_"),
                 "full_sku" => $s->{self::SKU_KEY},
-            ]),
+            ])->toArray(),
             manipulation_cost: 0, //todo is there manipulation cost?
             marked_as_new: array_search("New", $this->mapXml(fn($i) => (string) $i->name, $product->productTheme->flags)) !== false,
         );
