d5ab058c3d5423f83c38a1bfe1f4a20ec03e99b4
wpwwhimself <contact@wpww.pl>
2026-07-07 18:04:54 +0200

    📂👔 removed website data from mail footer

--- ofertownik/resources/views/layouts/mail.blade.php
+++ ofertownik/resources/views/layouts/mail.blade.php
@@ -1,1 +1,25 @@
-@extends("layouts.shipyard.mail")
+@extends("layouts.shipyard.simple")
+
+@section("body")
+
+<x-shipyard.app.big.header>
+    <x-slot:middle>
+        <h1 style="margin: 0;">@yield("title")</h1>
+    </x-slot:middle>
+</x-shipyard.app.big.header>
+
+<div id="middle-wrapper">
+    @hasSection("content")
+    <main>
+        @yield("content")
+    </main>
+    @endif
+</div>
+
+<x-shipyard.app.big.footer>
+    <x-slot:bottom>
+        {{-- <x-shipyard.mail.app-badge /> --}}
+    </x-slot:bottom>
+</x-shipyard.app.big.footer>
+
+@endsection
